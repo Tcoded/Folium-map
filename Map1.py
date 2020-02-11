84a736f7f63f08c809bd0f983e19d74e792c7632
Joshua Trevena <trevenacoded@gmail.com>
2020-02-10 22:44:19 -0800

    popups now display Google search for feature name and elevation

--- Map1.py
+++ Map1.py
@@ -2,20 +2,29 @@ import folium
 import pandas
 
 map = folium.Map(
-    location=[37.34, -121.89],
+    location=[37.34, -121.89], #San Jose, CA
     zoom_start=7,
-    titles="Stamen Terrain") #San Jose, CA
+    tiles="Stamen Terrain") 
 
-volcanoDataframe = pandas.read_csv("Volcanoes.txt")
-volLat = list(volcanoDataframe["LAT"])
-volLon = list(volcanoDataframe["LON"])
+volcanicDataframe = pandas.read_csv("Volcanoes.txt")
+volName = list(volcanicDataframe["NAME"])
+volLat = list(volcanicDataframe["LAT"])
+volLon = list(volcanicDataframe["LON"])
+volElev = list(volcanicDataframe["ELEV"])
 
 featG = folium.FeatureGroup(name="Volcanoes")
 
-for lat, lon in zip(volLat, volLon):
+for name, lat, lon, elev in zip(volName, volLat, volLon, volElev):
+    iframe = folium.IFrame(
+        html=f"<a href='https://www.google.com/search?q=%22{name}%22'"
+        f"target='_blank'>{name}</a>"
+        f"<br>"
+        f"{elev} m",
+        width=200,
+        height=100)
     featG.add_child(
         folium.Marker([lat, lon],
-        popup="VOLCANO",
+        popup=folium.Popup(iframe),
         icon=folium.Icon(color='red')))
 
 map.add_child(featG)
