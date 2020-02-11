dd14c6c58c51854695c7204ba47b75f05d7868df
Joshua Trevena <trevenacoded@gmail.com>
2020-02-10 17:54:14 -0800

    abstract marker to feat group

--- Map1.py
+++ Map1.py
@@ -5,9 +5,12 @@ map = folium.Map(
     zoom_start=7,
     titles="Stamen Terrain") #San Jose, CA
 
-map.add_child(
+featG = folium.FeatureGroup(name="My Map")
+featG.add_child(
     folium.Marker([37.44, -122.14],
     popup="Palo Alto, CA",
     icon=folium.Icon(color='purple')))
+
+map.add_child(featG)
 
 map.save("Map1.html")
