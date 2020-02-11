32d11199f70ed28d47fd7a043243de66e071ab35
Joshua Trevena <trevenacoded@gmail.com>
2020-02-10 17:45:23 -0800

    adjusted zoom and tileset

--- Map1.py
+++ Map1.py
@@ -1,4 +1,4 @@
 import folium
 
-map = folium.Map(location=[37.34, -121.89]) #San Jose, CA
+map = folium.Map(location=[37.34, -121.89], zoom_start=7, titles="Stamen Terrain") #San Jose, CA
 map.save("Map1.html")
