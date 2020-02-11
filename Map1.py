3891655b5d53cb8fd2c13f52a54dcba547a9aaee
Joshua Trevena <trevenacoded@gmail.com>
2020-02-10 18:35:03 -0800

    populate map with volcano coordinate markers
    dynamic marker info coming next

--- Map1.py
+++ Map1.py
@@ -1,15 +1,22 @@
 import folium
+import pandas
 
 map = folium.Map(
     location=[37.34, -121.89],
     zoom_start=7,
     titles="Stamen Terrain") #San Jose, CA
 
-featG = folium.FeatureGroup(name="My Map")
-featG.add_child(
-    folium.Marker([37.44, -122.14],
-    popup="Palo Alto, CA",
-    icon=folium.Icon(color='purple')))
+volcanoDataframe = pandas.read_csv("Volcanoes.txt")
+volLat = list(volcanoDataframe["LAT"])
+volLon = list(volcanoDataframe["LON"])
+
+featG = folium.FeatureGroup(name="Volcanoes")
+
+for lat, lon in zip(volLat, volLon):
+    featG.add_child(
+        folium.Marker([lat, lon],
+        popup="VOLCANO",
+        icon=folium.Icon(color='red')))
 
 map.add_child(featG)
 
