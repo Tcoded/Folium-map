5f0fd7459f3a673101bdb99be93c641be5eb1a19
Joshua Trevena <trevenacoded@gmail.com>
2020-02-10 17:50:55 -0800

    first icon added

--- Map1.py
+++ Map1.py
@@ -1,4 +1,13 @@
 import folium
 
-map = folium.Map(location=[37.34, -121.89], zoom_start=7, titles="Stamen Terrain") #San Jose, CA
+map = folium.Map(
+    location=[37.34, -121.89],
+    zoom_start=7,
+    titles="Stamen Terrain") #San Jose, CA
+
+map.add_child(
+    folium.Marker([37.44, -122.14],
+    popup="Palo Alto, CA",
+    icon=folium.Icon(color='purple')))
+
 map.save("Map1.html")
